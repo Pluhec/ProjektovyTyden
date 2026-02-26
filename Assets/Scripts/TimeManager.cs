a0fef60fa8b78b40699b6fd48401e7f84276d882
TomasZapletal23084 <23084tz@creativehill.cz>
2026-02-26 10:43:37 +0100

    DayNightCycle

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,9 +1,14 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
     SimulationHandler simulationHandler;
     public uint day;
+    public Image[] allMapResources;
+    public Gradient dayNightGradient;
+    private float timer;
 
     void Start()
     {
@@ -13,6 +18,18 @@ public class TimeManager : MonoBehaviour
     {
         if (simulationHandler == null) return;
 
+        timer += Time.deltaTime;
+        if (timer >= 1f)
+        {
+            timer = 0f;
+            simulationHandler.Tick();
+            simulationHandler.simulationTime++;
+        }
+
         day = simulationHandler.simulationTime / 60 + 1;
+        float timeOfTheDay = Mathf.Pow(Mathf.Cos((simulationHandler.simulationTime % 60 + timer) / 60f * Mathf.PI * 2)*0.5f+0.5f, 0.28f);
+        print(timeOfTheDay);
+        foreach (var img in allMapResources)
+            img.color = dayNightGradient.Evaluate(timeOfTheDay);
     }
 }
